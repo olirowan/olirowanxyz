93acac4a60e9d0bd153022ef6aab54963a2e0b80
Oli Rowan <olirowan@gmail.com>
2019-01-13 15:37:49 +0000

    Forgot to wrap ID in a string

--- app/validate_admin.py
+++ app/validate_admin.py
@@ -1,6 +1,6 @@
 from app import app
 
-blog_admin_id = str(app.config['BLOG_ADMIN_ID'])
+blog_admin_id = app.config['BLOG_ADMIN_ID']
 blog_admin_user = app.config['BLOG_ADMIN_USER']
 
 
@@ -8,7 +8,7 @@ def validate_if_admin_user(current_user):
 
     app.logger.info("Admin validation requested. ID: " + str(current_user.id) + " Username: " + current_user.username)
 
-    if (current_user.id) == blog_admin_id and (current_user.username) == blog_admin_user:
+    if (current_user.id) == str(blog_admin_id) and (current_user.username) == blog_admin_user:
         app.logger.info("Admin validation successful for user: " + current_user.username)
         return True
     else:
