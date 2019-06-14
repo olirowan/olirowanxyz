8aa01717832295c6976c49fbdbce0ea6e5a660e6
Oli Rowan <olirowan@gmail.com>
2019-06-14 20:11:17 +0100

    Modifying default table tr css properties

--- orxyz_setup.sh
+++ orxyz_setup.sh
@@ -5,9 +5,8 @@ python -m venv venv
 venv/bin/pip install -r /app/olirowanxyz/requirements.txt
 source venv/bin/activate
 mkdir /tmp/olirowanxyz
-cd /tmp/olirowanxyz
-flask db init
-flask db migrate -m "Initial Database State"
-flask db upgrade
 cd /app/olirowanxyz
+flask db init --directory /tmp/olirowanxyz/migrations
+flask db migrate -m "Initial Database State" --directory /tmp/olirowanxyz/migrations
+flask db upgrade --directory /tmp/olirowanxyz/migrations
 exec gunicorn -b :8000 --access-logfile - --error-logfile - olirowanxyz:app
